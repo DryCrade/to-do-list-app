7c5ae8337ae5e0d059d86b25255d5e55a53a79a8
Dennis William <denniswilliam273@gmail.com>
2025-03-19 00:56:50 +0700

    dockerized

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/to-do-list-app"
+  base: "/to-do-list-app",
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: 8080,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  }
 })
